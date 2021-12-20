0a2bc0e6624e895ca9a21085ace83a7a79d89287
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2021-12-20 17:44:02 +0530

    hosting changes

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0c*n(u$b41s!x(v5j=waoei+bs&9wq^9x$0%p%8=&-3ec5_xga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-dev-search.herokuapp.com']
 
 
 # Application definition
